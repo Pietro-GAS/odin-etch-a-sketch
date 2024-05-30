3615819f9fcb562cbd0f21579b5344c8788c7263
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2024-05-30 22:30:11 +0200

    Extend to 16*16

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 const body = document.querySelector("body");
 const container = document.querySelector("#container");
 for (let i = 1; i <= 16; i++) {
-    let name = `cell${i}`;
-    const div = document.createElement("div");
-    div.setAttribute("id", name);
-    div.textContent = name;
-    container.appendChild(div);
+    for (let j = 1; j <= 16; j++) {
+        let name = `cell${i}${j}`;
+        const div = document.createElement("div");
+        div.setAttribute("id", name);
+        div.textContent = name;
+        container.appendChild(div);
+    }
 }
 
 body.appendChild(container);
